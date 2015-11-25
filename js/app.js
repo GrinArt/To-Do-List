4c47b666203fd8908ec634407f97dafcbe9b39a7
Artem Grin <wtf.noword@gmail.com>
2015-11-25 13:56:24 +0200

    Returns the old value of the edited problem with loss of focus

--- js/app.js
+++ js/app.js
@@ -6,27 +6,16 @@ var taskList__todo = $(".taskList--todo").eq(0);
 var taskList__done = $(".taskList--done").eq(0);
 var tasks = $(".taskItem");
 
-
-$(".taskList--todo, .taskList--done").sortable({
-
-  connectWith: ".taskList",
-  change: function(event,ui) {
-      if (ui.sender) {
-        moveTask.call(ui.item[0].querySelector('input[type=checkbox]'));
-      }
-  }
-
-}).disableSelection();
+var nId = 0;
+var tdMask = 'td_';
+var lsMas = [];
 
 
-var addTask = function () {
-  
-  if (!taskInput.val()) return;
-  
-  var $taskItem = $(
-    '<li class="taskItem clearfix">' +
+var createTask = function(val, id) {
+  var taskItem = $(
+    '<li class="taskItem clearfix" data-itemid="' + id + '">' +
       '<input type="checkbox" class="taskSwitch">' +
-      '<label class="taskContent">' + taskInput.val() + '</label>' +
+      '<label class="taskContent">' + val + '</label>' +
       '<input type="text" class="taskInput">' +
       '<span class="taskItemControl layout--pushRight">' +
         '<button class="editBtn">Редактировать</button> ' +
@@ -34,12 +23,41 @@ var addTask = function () {
       '</span>' +
     '</li>'
   );
-  taskList__todo.append($taskItem);
+
+  return taskItem;
+};
+
+var addTask = function (source, status, val, id) {
+  
+  if (source != 'storage') {
+    if (!taskInput.val()) return;
+    
+    val = taskInput.val();
+    
+    localStorage.setItem(
+      tdMask + nId,
+      '{"value": "'+ val + '","isDo": "false" }'
+    );
+    
+    var taskItem = createTask(val, tdMask + nId);
+    taskList__todo.append(taskItem);
+  } else {
+    var taskItem = createTask(val, id);
+
+    taskList__todo.append(taskItem);
+    
+    if (status == 'true') {
+      moveTask.call(taskItem.find('.taskSwitch'));
+    }
+  }
 
   taskInput.val('');
 
-  bindTaskEvents.call($taskItem);
-}; addBtn.click(addTask);
+  bindTaskEvents.call(taskItem);
+}; addBtn.on('click', function () {
+  addTask();
+  nId++;
+});
 
 var moveTask = function () {
   var taskItem = $(this).parent();
@@ -47,14 +65,26 @@ var moveTask = function () {
   var _taskList__todo = taskList.hasClass("taskList--todo");
   var _taskList__done = taskList.hasClass("taskList--done");  
   
+  var value = taskItem.find('.taskContent').html();
+  
   if (_taskList__todo) {
     taskItem.find(".taskContent").toggleClass("checkboxTrue");
     taskItem.find(".taskSwitch").prop("checked", false);
     taskList__done.append(taskItem);
+    
+    localStorage.setItem(
+      taskItem.attr("data-itemid"),
+      '{"value":"'+value+'","isDo":"true"}'
+    );
   } else if (_taskList__done) {
     taskItem.find(".taskContent").toggleClass("checkboxTrue");
     taskItem.find(".taskSwitch").prop("checked", true);
     taskList__todo.append(taskItem);
+    
+    localStorage.setItem(
+      taskItem.attr("data-itemid"),
+      '{"value":"'+value+'","isDo":"false"}'
+    );
   }
   
   taskItem.find(".editBtn").toggleClass("is-hidden");
@@ -110,7 +140,12 @@ $(document).mousedown(function(e) {
 
 var blurInput = function(e) {
   if (!clicky.eq(0).hasClass('editBtn')) {
-    console.log('eto ne edit BTn');
+    console.log("q");
+    $(this).removeClass("is-visible");
+    $(this).addClass("is-hidden");
+    $(this).parent().find('.taskContent').removeClass("is-hidden");
+    $(this).parent().find('.editBtn').html("Редактировать");
+    $(this).parent().find('.editBtn').prop("disabled", false);
   }
 }
 
@@ -127,3 +162,40 @@ var bindTaskEvents = function () {
 for (var i = 0; i < tasks.length; i++) {
   bindTaskEvents.call(tasks.eq(i));
 }
+
+$(".taskList--todo, .taskList--done").sortable({
+  cancel: "li:has(label.is-hidden)",
+  connectWith: ".taskList",
+  change: function(event,ui) {
+      if (ui.sender) {
+        moveTask.call(ui.item[0].querySelector('input[type=checkbox]'));
+      }
+  }
+}).disableSelection();
+
+function showTask() {
+  var localLength = [];
+  
+  for(var key in localStorage) {
+    console.log(key);
+    if (key.slice(0,3) === tdMask){
+      localLength.push(key);
+    }
+  }
+  nId = localLength.length;
+
+  localLength.sort(function(a,b){
+    return a.slice(3)-b.slice(3);
+  });
+
+
+  for (var i = 0; i < localLength.length; i++) {
+
+    var key = localLength[i];
+    var val = JSON.parse(localStorage[key]);
+
+    addTask('storage', val.isDo, val.value, key);
+  }
+}
+
+showTask();
